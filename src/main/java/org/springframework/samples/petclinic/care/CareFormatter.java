16541937a6cf35def28b0154f6b7807c72615252
alvnavriv <alvnavriv@alum.us.es>
2022-12-22 12:18:01 +0100

    Entrega EJ 1-5

--- src/main/java/org/springframework/samples/petclinic/care/CareFormatter.java
+++ src/main/java/org/springframework/samples/petclinic/care/CareFormatter.java
@@ -6,19 +6,28 @@ import java.util.Locale;
 import org.springframework.format.Formatter;
 import org.springframework.stereotype.Component;
 
+import lombok.AllArgsConstructor;
+
 @Component
+@AllArgsConstructor
 public class CareFormatter implements Formatter<Care>{
+
+    CareService careService;
 
     @Override
     public String print(Care object, Locale locale) {
         // TODO Auto-generated method stub
-        return null;
+        return object.getName();
     }
 
     @Override
     public Care parse(String text, Locale locale) throws ParseException {
         // TODO Auto-generated method stub
-        return null;
+        Care care = careService.getCare(text);
+        if(care == null){
+            throw new ParseException("Care not found: "+ text,0);
+        }
+        return care;
     }
     
 }
